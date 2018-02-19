2b3a68dfa69a544ee6fb527f0cd93148e23b3061
Jay Pan <sfffaaa@gmail.com>
2018-02-19 17:26:49 +0100

    Remove unused test code written by solidity and change part of test code by ES6

--- test/will.js
+++ test/will.js
@@ -1,25 +1,19 @@
 var Will = artifacts.require("Will");
+const Web3 = require('web3')
 var TEST_USER_DATA = "aabbccddeeff";
 
 contract('Will', function(accounts) {
-    it("Retrieve (A)", function() {
-        return Will.deployed().then(function(instance) {
-            return instance.Retrieve.call(accounts[0]);
-        }).then(function(userdata) {
-            assert.equal(userdata, "", "accounts not exist, so return empty data");
-        });
+    it("Retrieve (A)", async function() {
+        var instance = await Will.deployed();
+        var userdata = await instance.Retrieve.call(accounts[0]);
+        assert.equal(userdata, "", "accounts not exist, so return empty data");
     });
 
-    it("Delete (A) + Retrieve (A)", function() {
-        var contract;
-        return Will.deployed().then(function(instance) {
-            contract = instance;
-            return instance.Delete(accounts[0]);
-        }).then(function(result) {
-            return contract.Retrieve.call(accounts[0]);
-        }).then(function(userdata) {
-            assert.equal(userdata, "", "accounts not exist, so return empty data");
-        });
+    it("Delete (A) + Retrieve (A)", async function() {
+        var contract = await Will.deployed();
+        var result = await contract.Delete(accounts[0]);
+        var userdata = await contract.Retrieve.call(accounts[0]);
+        assert.equal(userdata, "", "accounts not exist, so return empty data");
     });
 });
 
